97552c1b6512cb592f12dc9351c2fa49a04f0fe3
jyotiraj007 <jyotirajsingh3@gmail.com>
2020-01-15 17:10:25 +0530

    graph representation using adjacency list improved

--- logical-datastructures/graphs/representation/main.py
+++ logical-datastructures/graphs/representation/main.py
@@ -3,13 +3,6 @@ from vertex import Vertex
 
 graph = Graph()
 
-v1 = Vertex('one')
-v2 = Vertex('two')
-v3 = Vertex('three')
-v4 = Vertex('four')
-v5 = Vertex('five')
-v6 = Vertex('six')
-
 for i in range(ord('A'), ord('E')):
     graph.add_vertices(Vertex(chr(i)))
 
